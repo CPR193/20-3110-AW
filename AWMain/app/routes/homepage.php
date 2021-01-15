1b3f0b6077e3f777a7c4e924161d848746661f0f
edsstep <77497356+edsstep@users.noreply.github.com>
2021-01-15 16:34:18 +0000

    Add files via upload

---
+++ AWMain/app/routes/homepage.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Home page of the application
+ */
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/', function(Request $request, Response $response) {
+    $html_output = $this->view->render($response,
+    'homepageform.html.twig',
+    [
+        'css_path' => CSS_PATH,
+        'landing_page' => LANDING_PAGE,
+        'method' => 'get',
+        'action' => 'index.php',
+        'page_title' => APP_NAME,
+        'page_heading_1' => 'Hello yes, this is 20-3110-AW.',
+        'page_text' => ':) Hello world or something, IDK I\'m not a computer. Please give us extension!'
+    ]);
+
+    return $html_output;
+})->setName('homepage');
---
+++ AWMain/app/src/SQLQueries.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace AWMain;
+
+
+class SQLQueries
+{
+    public function __construct(){}
+    public function __destruct(){}
+}
