31f8520c7c7d9d81955375821b75d13625c5ff4a
edsstep <77497356+edsstep@users.noreply.github.com>
2021-01-15 16:40:08 +0000

    Add files via upload

---
+++ phpappfolder/public_php/AWMain/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * bootstraop file which links the app path, settings path and then the settings themselves,
+ * the dependencies file and the routes file.
+ *
+ * @package AWMain
+ */
+
+session_start();
+
+require 'vendor/autoload.php';
+
+$app_path   = __DIR__ . '/app/';
+$settings   = require $app_path . 'settings.php';
+$container  = new \Slim\Container($settings);
+
+require $app_path . 'dependencies.php';
+
+$app = new \Slim\App($container);
+
+require $app_path . 'routes.php';
+
+$app->run();
