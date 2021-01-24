490fbf0bc80abc1b4a91403c1927022849a5f94b
Cosmin <60136940+CPR193@users.noreply.github.com>
2021-01-24 22:56:21 +0000

    Update 5 - Added Comments

--- phpappfolder/public_php/AWMain/index.php
+++ phpappfolder/public_php/AWMain/index.php
@@ -2,7 +2,8 @@
 /**
  * Index of the application that redirects to the bootstrap
  *
- * @package AWMain
+ * @package AWMain;
+ * @author 20-3110-AW - Cosmin
  */
 
 
@@ -18,7 +19,7 @@ if ($make_trace == true && function_exists(xdebug_start_trace())) {
 }
 
 
-include_once 'AWMain/bootstrap.php';
+include 'AWMain/bootstrap.php';
 
 if ($make_trace == true && function_exists(xdebug_stop_trace())) {
     xdebug_stop_trace();
